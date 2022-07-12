3d551ff2718d5ca3698acf1135980a2ac768d91f
splash <splash@hgraph.io>
2022-07-12 07:43:09 -0600

    get auth token from client_id/client_secret pair & make a call to https://hgraph.io/v1/graphql

---
+++ README.md
@@ -0,0 +1,6 @@
+# hgraph.io - server-side authentication example
+
+## Usage
+- `cp .env.example .env`
+- enter client secret and client id in .env
+- `npm run example`
---
+++ index.js
@@ -0,0 +1,47 @@
+import fs from 'fs'
+import * as dotenv from 'dotenv'
+import got from 'got'
+
+dotenv.config()
+
+async function main() {
+  /*
+   * Authentication
+   */
+  const authEndpoint = 'https://auth.hgraph.io/oauth/token'
+  const authResponse = await got
+    .post(authEndpoint, {
+      json: {
+        client_id: process.env.HGRAPH_CLIENT_ID,
+        client_secret: process.env.HGRAPH_CLIENT_SECRET,
+        audience: 'https://api.hgraph.io',
+        grant_type: 'client_credentials',
+      },
+    })
+    .json()
+
+  /*
+   * hgraph query
+   */
+  const query = fs.readFileSync('lib/SampleHgraphQuery.gql', 'utf8')
+  const accountId = 3
+
+  const response = await got
+    .post('https://api.hgraph.io/v1/graphql', {
+      headers: {
+        Authorization: `Bearer ${authResponse.access_token}`,
+      },
+      json: {
+        query,
+        variables: {
+          accountId,
+        },
+      },
+    })
+    .json()
+
+  console.table(response.data.account_balance)
+  //console.log(JSON.stringify(response, null, 2))
+}
+
+main()
